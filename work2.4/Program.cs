b71f6b4dbdf2f78195771eed8dc364aafa23e543
MansurKing <102825225+MansurKing@users.noreply.github.com>
2022-04-06 18:21:38 +0300

    init

---
+++ work2.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace work2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                i = rand.Next(0, 16);
+
+                if (i % 5 == 0 || i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                    Console.ReadKey();
+                }
+
+            }
+
+        }
+    }
+}
